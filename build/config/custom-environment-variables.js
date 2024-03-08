c602e798195cf0c44c248fafc408ca4efedeeabf
ngaridennis33 <ngaridennis33@gmail.com>
2024-03-08 18:42:19 +0300

    Updated the env

---
+++ build/config/custom-environment-variables.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    port: "PORT",
+    accessTokenPrivateKey: "JWT_ACCESS_TOKEN_PRIVATE_KEY",
+    accessTokenPublicKey: "JWT_ACCESS_TOKEN_PUBLIC_KEY",
+    refreshTokenPrivateKey: "JWT_REFRESH_TOKEN_PRIVATE_KEY",
+    refreshTokenPublicKey: "JWT_REFRESH_TOKEN_PUBLIC_KEY",
+    smtp: {
+        service: 'EMAIL_SERVICE',
+        host: 'EMAIL_HOST',
+        pass: 'EMAIL_PASS',
+        port: 'EMAIL_PORT',
+        user: 'EMAIL_USER',
+        secure: 'EMAIL_STATUS',
+    },
+};
